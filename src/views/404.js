cfbb70e81860ddc39ff52a1f277a748cb2f96cea
Super <linzechao44@163.com>
2018-07-08 13:36:05 +0800

    [UPDATE] add 404

--- src/views/404.js
+++ src/views/404.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Index = () => {
-  return <div>404.</div>
+  return <div style={{
+    lineHeight: '200px',
+    fontSize: '40px',
+    textAlign: 'center',
+    color: '#ddd'
+  }}>404 Not Found.</div>
 }
 
 export default Index
