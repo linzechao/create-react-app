53e8bfc6dd01d4b2615cc7c2f101bb1c9ba4c4d8
Super <linzechao44@163.com>
2018-07-08 11:02:34 +0800

    [UPDATE] 调整项目目录

---
+++ src/views/course/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { NavBar, Icon } from 'antd-mobile'
+
+const Index = () => {
+  return (
+    <NavBar
+      mode="dark"
+      leftContent="Back"
+      rightContent={[
+        <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >NavBar</NavBar>
+  )
+}
+
+export default Index
---
+++ src/views/user/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index = () => {
+  return <div>User Index</div>
+}
+
+export default Index
